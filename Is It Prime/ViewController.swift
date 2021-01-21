3bc39164a07b79cacad88bafac991e63f02dc4ab
alanrmendez <alan.mendez07@gmail.com>
2021-01-21 15:19:14 -0500

    Initial Commit

---
+++ Is It Prime/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift.
+//  Is It Prime
+//
+//  Created by Alan Mendez on 1/21/21.
+//  
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
