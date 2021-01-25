800789d02102699be8412bdc12fb04952992e05e
alanrmendez <alan.mendez07@gmail.com>
2021-01-25 12:59:40 -0500

    Added touchesBegan override and resign function so that the user can vanish the keyboard appropriately.

--- Is It Prime/ViewController.swift
+++ Is It Prime/ViewController.swift
@@ -8,13 +8,62 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var numberTextField: UITextField!
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    @IBAction func numberEntered(_ sender: Any) {
+        
+        if let userEnteredNumber = numberTextField.text {
+            
+            let userInteger = Int(userEnteredNumber)
+            if let number = userInteger {
+                
+                var isPrime = true
+                
+                if number == 1 {
+                    //number is not prime
+                    isPrime = false
+                }
+                
+                var i = 2
+                while i < number {
+                    if number % i == 0 {
+                        isPrime = false
+                        }
+                    i += 1
+                    }
+                if isPrime == true {
+                    numberLabel.text = "\(number) is prime!"
+                } else{
+                    numberLabel.text = "\(number) is not prime."
+                }
+                
+        }
+        else {
+            numberLabel.text = "Please enter a positive whole number."
+        }
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    //make keyboard disappear when elsewhere on the screen is pressed
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    
+    //make keyboard disappear when return button is pressed
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 
 }
 
